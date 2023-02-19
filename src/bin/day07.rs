38565aba80a507630426e11097d6443c9df54569
rintanen <perttu.rintanen@ouraring.com>
2023-02-19 18:21:04 +0200

    fars

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,6 +1,35 @@
-use std::fs;
+use std::collections::{HashMap, HashSet};
+use std::path::PathBuf;
+
+fn dir_size() -> u32 {
+    0
+}
+
 
 fn main()  {
-    let task_input = fs::read_to_string("inputs/day07.in").expect("fars");
-    task_input.split("$");
+    let task_input = include_str!("../../inputs/day07.in");
+    let mut cwd = PathBuf::new();
+    let mut file_tree: HashMap<PathBuf, u32> = HashMap::new();
+
+    for chunk in task_input.split("$").skip(1) {
+        let command = chunk.lines().next().unwrap().trim();
+        // println!("l = ({})", command);
+        match command {
+            "cd .." => {
+                cwd.pop();
+            },
+            "ls" => {
+                // rekursiivisesti laske dir size ja laita value othon
+                let size = dir_size();
+                file_tree.insert(cwd.clone(), size);
+            },
+            // this matches any pattern, so catches the "cd <dir_name>" case
+            cd => {
+                cwd.push(cd.split_once(" ").unwrap().1);
+            }
+        }
+    }
+
+    println!("{:?}", cwd);
+    
 }
